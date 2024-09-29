c09466b4b550d202221a69057ff5ff92e795efaa
vanhlion <22010341@st.phenikaa-uni.edu.vn>
2024-09-29 14:36:31 +0700

    Trang danh sách sân

---
+++ resources/Danh_sach_san/JS/menu.js
@@ -0,0 +1,8 @@
+document.querySelector('.openmenu').addEventListener('click', function() {
+    document.querySelector('.overlay').classList.remove('hidden');
+});
+document.querySelector('.overlay').addEventListener('click', function(event) {
+    this.classList.add('hidden');
+    event.stopPropagation(); // Ngăn sự kiện lan ra ngoài
+});
+
---
+++ resources/Danh_sach_san/JS/typeField.js
@@ -0,0 +1,4 @@
+document.getElementById('toggleBar').addEventListener('click', function() {
+    var menuItems = document.getElementById('menuItems');
+    menuItems.classList.toggle('d-none');
+});
